e59d3928fb95492853069480e4f1f165468eef9c
David Read <david.read@digital.justice.gov.uk>
2024-02-09 09:34:45 +0000

    Fix save/load of data

--- frontend_app/app.py
+++ frontend_app/app.py
@@ -28,15 +28,19 @@ def hello_world():
 @app.route("/monolith", methods=['GET', 'POST'])
 def form1():
     form = CaseForm(request.form)
+    case = db.session.scalars(db.select(Case).limit(1)).first()
     if request.method == 'POST' and form.validate():
-        case = Case(name=form.name.data)
-        db.session.add(case)
+        if not case:
+            # create case
+            case = Case(name=form.name.data)
+            db.session.add(case)
+        else:
+            # amend existing case
+            case.name = form.name.data
         db.session.commit()
         flash('Saved!')
-    else:
-        case = db.session.execute(db.select(Case)).first()
-        if case:
-            form.name.data = case[0].name
+    elif case:
+        form.name.data = case.name
     return render_template('form.html', form=form)
 
 @app.route("/flask_and_drf")
